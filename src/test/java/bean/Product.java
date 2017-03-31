68041966cfac5250e39860e6b31ab61055a46cb6
MaryiaChuhunova <Maryia_Chuhunova@epam.com>
2017-03-31 22:27:35 +0400

    Create Product.java

---
+++ src/test/java/bean/Product.java
@@ -0,0 +1,50 @@
+package bean;
+
+/**
+ * Created by Maria on 31.03.2017.
+ */
+public class Product {
+
+    private Integer id;
+    private String name;
+    private Double price;
+
+    public Product() {}
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean hasEmptyFields() {
+        if((name == "") || (price == 0)) {
+            return true;
+        }
+        return false;
+    }
+}
